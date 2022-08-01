547e0b5a086b670ba7b6747277beb964528bf18f
Anton Rubchenko <rubchenko.anton@gmail.com>
2022-08-01 12:52:31 +0300

    chore: add props for Card component&fix category btn

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Card } from './components'
 const App = () => {
   return (
     <div className='App'>
-      <Card />
-      <Card />
+      <Card title={'Add Buton'} description={'123'} comment={1} category={'UI'} vote={12} />
+      <Card title={'Add tags'} description={'1234'} comment={2} category={'Enhancement'} vote={13}/>
     </div>
   )
 }
