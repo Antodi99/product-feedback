5b151f7e6249acf4ff433aa7be32f620c67cc203
Anton Rubchenko <48853826+Antodi99@users.noreply.github.com>
2022-08-04 16:38:00 +0300

    US011: add NoFeedback component (#13)
    * chore: add NoFeedback component
    
    * fix: linting
    
    Co-authored-by: Arthur Ruchenko <8872307@gmail.com>

---
+++ src/components/NoFeedback.tsx
@@ -0,0 +1,23 @@
+import { AiOutlineFileSearch } from 'react-icons/ai'
+
+function NoFeedback() {
+  return (
+    <div className='bg-white flex flex-col rounded-lg p-16 md:p-60 h-fit w-full justify-center items-center'>
+      <AiOutlineFileSearch className='text-8xl text-dark-grey' />
+      <div className='flex justify-center text-center flex-col mt-8 max-w-3/5'>
+        <h1 className='text-dark-blue text-lg font-bold'>
+          There is no feedback yet.
+        </h1>
+        <p className='text-dark-blue text-sm mt-4'>
+          Got a suggestion? Found a bug that needs to be squashed? We love
+          hearing about new ideas to improve our app.
+        </p>
+      </div>
+      <div className='rounded-xl p-3 bg-fuchsia-500 w-fit flex items-center justify-center hover:bg-fuchsia-400 cursor-pointer mt-6'>
+        <p className='text-white font-bold text-sm'>+Add Feedback</p>
+      </div>
+    </div>
+  )
+}
+
+export default NoFeedback
