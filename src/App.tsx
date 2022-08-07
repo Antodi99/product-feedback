ceffc0ec4f71109d852a3bf818cca58b344704ba
Anton Rubchenko <48853826+Antodi99@users.noreply.github.com>
2022-08-08 00:28:14 +0300

    US017: add RoadmapPage component(without mobile) (#19)
    * chore: add RoadmapPage component(without mobile)
    
    * chore: fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import MainPage from './components/MainPage'
 import { Routes, Route } from 'react-router-dom'
 import FeedbackPage from './components/FeedbackPage'
+import RoadmapPage from './components/RoadmapPage'
 import AddFeedbackPage from './components/AddFeedbackPage'
 import EditFeedbackPage from './components/EditFeedbackPage'
 
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<MainPage />} />
       <Route path='/feedback/:id' element={<FeedbackPage />} />
+      <Route path='/roadmap' element={<RoadmapPage />} />
       <Route path='/feedback/add' element={<AddFeedbackPage />} />
       <Route path='/feedback/:id/edit' element={<EditFeedbackPage />} />
     </Routes>
