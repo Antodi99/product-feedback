022e741dfbb41171506204c7c82b45b1323b8fe7
Anton Rubchenko <rubchenko.anton@gmail.com>
2022-08-07 22:21:08 +0300

    chore: fixes

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -3,6 +3,33 @@ import { Card } from './Card'
 import { Header } from './Header'
 import ManageBar from './ManageBar'
 
+const feedbackList = [
+  {
+    id: 1,
+    title: 'test title',
+    description: 'some description',
+    commentsCount: 3,
+    category: 'category',
+    votesCount: 34,
+  },
+  {
+    id: 1,
+    title: 'test title',
+    description: 'some description',
+    commentsCount: 3,
+    category: 'category',
+    votesCount: 34,
+  },
+  {
+    id: 1,
+    title: 'test title',
+    description: 'some description',
+    commentsCount: 2,
+    category: 'category',
+    votesCount: 34,
+  },
+]
+
 function MainPage() {
   return (
     <div className='flex flex-col lg:flex-row min-h-screen w-screen lg:justify-center md:p-11 lg:pt-20'>
@@ -10,29 +37,17 @@ function MainPage() {
       <main className='flex flex-col md:mt-8 lg:mt-0 lg:ml-8 w-full lg:w-5/12'>
         <ManageBar />
         <div className='px-4 pb-4 md:px-0 md:pb-0'>
-          <Link to={'/feedback/${feedback.id}'}>
-            <Card
-              title={'12312'}
-              description={'1231'}
-              comment={1231}
-              category={'1231'}
-              vote={121}
-            ></Card>
-          </Link>
-          <Card
-            title={'12312'}
-            description={'1231'}
-            comment={1231}
-            category={'1231'}
-            vote={121}
-          ></Card>
-          <Card
-            title={'12312'}
-            description={'1231'}
-            comment={1231}
-            category={'1231'}
-            vote={121}
-          ></Card>
+          {feedbackList.map((feedback) => (
+            <Link key={feedback.id} to={`/feedback/${feedback.id}`}>
+              <Card
+                title={feedback.title}
+                description={feedback.description}
+                comment={feedback.commentsCount}
+                category={feedback.category}
+                vote={feedback.votesCount}
+              ></Card>
+            </Link>
+          ))}
         </div>
       </main>
     </div>
