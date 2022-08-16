7e566308f6cdb82f13b24da4250f7b3eb91808da
Anton Rubchenko <rubchenko.anton@gmail.com>
2022-08-16 20:28:04 +0300

    chore: add MenuDropdown component

--- src/components/ManageBar.tsx
+++ src/components/ManageBar.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import { FaVoteYea, FaAngleDown } from 'react-icons/fa'
+import React, { useState } from 'react'
+import { FaVoteYea } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import MenuDropdown from './MenuDropdown'
 
 function ManageBar() {
+  const [selected, setSelected] = useState('Most Upvotes')
   return (
     <div className='bg-dark-blue md:rounded-lg p-4 flex items-center justify-between text-xs md:text-sm'>
       <div className='flex items-center w-4/5'>
@@ -10,13 +12,7 @@ function ManageBar() {
         <h1 className='text-lg font-bold md:ml-3 lg:ml-9 text-white hidden md:inline-block'>
           6 Suggestions
         </h1>
-        <button className='flex md:ml-3 lg:ml-9 text-white hover:text-dark-grey'>
-          <p className=''>Sort By :</p>
-          <div className='ml-2 lg:ml-4 flex font-bold items-center'>
-            <p className=''>Most Upvotes</p>
-            <FaAngleDown className='ml-2 md:ml-4 ' />
-          </div>
-        </button>
+        <MenuDropdown selected={selected} setSelected={setSelected} />
       </div>
       <Link to={'/feedback/add'}>
         <div className='rounded-xl p-2 md:p-3 bg-fuchsia-500 w-36 flex items-center justify-center hover:bg-fuchsia-400 cursor-pointer'>
