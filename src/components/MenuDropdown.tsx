2fe70198e65286516b9d89c5f6eb427c75a4c584
Anton Rubchenko <rubchenko.anton@gmail.com>
2022-08-17 10:16:06 +0300

    fix

--- src/components/MenuDropdown.tsx
+++ src/components/MenuDropdown.tsx
@@ -19,7 +19,7 @@ export function MenuDropdown({ selected, setSelected }: MenuDropdownProps) {
   const [isActive, setIsActive] = useState(false)
 
   return (
-    <Menu as='div' className='relative inline-block text-left ml-6'>
+    <Menu as='div' className='relative inline-block text-left md:ml-6'>
       <div>
         <Menu.Button
           className='inline-flex justify-center w-full rounded-md px-4 py-2 text-sm font-medium text-white'
