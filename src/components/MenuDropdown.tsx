c1fc4d575bf5030ba2f707a84826b8a301838430
Arthur Ruchenko <8872307@gmail.com>
2022-08-18 11:22:09 +0300

    fix: use open prop of headlessui Menu component

--- src/components/MenuDropdown.tsx
+++ src/components/MenuDropdown.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import { Fragment, useState } from 'react'
+import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 import { FaCheck } from 'react-icons/fa'
@@ -17,54 +17,50 @@ type MenuDropdownProps = {
 }
 
 export function MenuDropdown({ selected, setSelected }: MenuDropdownProps) {
-  const [open, setOpen] = useState(false)
-
   return (
     <Menu as='div' className='relative inline-block text-left md:w-52 md:ml-6'>
-      <div>
-        <Menu.Button
-          className='inline-flex w-full rounded-md px-4 py-2 text-sm font-medium text-white justify-between'
-          onClick={() => setOpen(!open)}
-        >
-          Sort by : {selected}
-          <ChevronDownIcon
-            className={clsx(
-              '-mr-1 h-5 w-5 ease-linear duration-150',
-              open && 'rotate-180'
-            )}
-            aria-hidden='true'
-          />
-        </Menu.Button>
-      </div>
-
-      <Transition
-        as={Fragment}
-        enter='transition ease-out duration-100'
-        enterFrom='transform opacity-0 scale-95'
-        enterTo='transform opacity-100 scale-100'
-        leave='transition ease-in duration-75'
-        leaveFrom='transform opacity-100 scale-100'
-        leaveTo='transform opacity-0 scale-95'
-      >
-        <Menu.Items className='origin-top-right absolute left-0 md:right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none'>
-          {options.map((option, idx) => (
-            <Menu.Item key={idx}>
-              <div
-                className='flex justify-between items-center px-4 py-3 cursor-pointer text-base text-dark-blue hover:text-violet-600'
-                onClick={() => {
-                  setSelected(option)
-                  setOpen(false)
-                }}
-              >
-                {option}
-                {selected === option && (
-                  <FaCheck className='text-violet-600 text-sm' />
+      {({ open }) => (
+        <>
+          <div>
+            <Menu.Button className='inline-flex w-full rounded-md px-4 py-2 text-sm font-medium text-white justify-between'>
+              Sort by : {selected}
+              <ChevronDownIcon
+                className={clsx(
+                  '-mr-1 h-5 w-5 ease-linear duration-150',
+                  open && 'rotate-180'
                 )}
-              </div>
-            </Menu.Item>
-          ))}
-        </Menu.Items>
-      </Transition>
+                aria-hidden='true'
+              />
+            </Menu.Button>
+          </div>
+
+          <Transition
+            as={Fragment}
+            enter='transition ease-out duration-100'
+            enterFrom='transform opacity-0 scale-95'
+            enterTo='transform opacity-100 scale-100'
+            leave='transition ease-in duration-75'
+            leaveFrom='transform opacity-100 scale-100'
+            leaveTo='transform opacity-0 scale-95'
+          >
+            <Menu.Items className='origin-top-right absolute left-0 md:right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none'>
+              {options.map((option, idx) => (
+                <Menu.Item key={idx}>
+                  <div
+                    className='flex justify-between items-center px-4 py-3 cursor-pointer text-base text-dark-blue hover:text-violet-600'
+                    onClick={() => setSelected(option)}
+                  >
+                    {option}
+                    {selected === option && (
+                      <FaCheck className='text-violet-600 text-sm' />
+                    )}
+                  </div>
+                </Menu.Item>
+              ))}
+            </Menu.Items>
+          </Transition>
+        </>
+      )}
     </Menu>
   )
 }
