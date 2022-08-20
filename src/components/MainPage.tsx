1709a13b1a03a32a227a08ea3ff599ef5b9eea44
Anton Rubchenko <48853826+Antodi99@users.noreply.github.com>
2022-08-20 19:48:10 +0300

    US018: Add logic to 'Sort By' filter(MainPage) (#26)
    * chore: Add logic to 'Sort By' filter(MainPage)
    
    * chore: fix

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,9 +1,19 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Card } from './Card'
 import { Header } from './Header'
 import ManageBar from './ManageBar'
 
-const feedbackList = [
+type Feedback = {
+  id: number
+  title: string
+  description: string
+  commentsCount: number
+  category: string
+  votesCount: number
+}
+
+const feedbackList: Feedback[] = [
   {
     id: 1,
     title: 'test title',
@@ -18,7 +28,7 @@ const feedbackList = [
     description: 'some description',
     commentsCount: 3,
     category: 'category',
-    votesCount: 34,
+    votesCount: 36,
   },
   {
     id: 3,
@@ -26,18 +36,44 @@ const feedbackList = [
     description: 'some description',
     commentsCount: 2,
     category: 'category',
-    votesCount: 34,
+    votesCount: 35,
   },
 ]
 
+function sortByVotesAndComments(sortByFilter: string) {
+  return (a: Feedback, b: Feedback) => {
+    switch (sortByFilter) {
+      case 'Most Upvotes':
+        return b.votesCount - a.votesCount
+      case 'Least Upvotes':
+        return a.votesCount - b.votesCount
+      case 'Most Comments':
+        return b.commentsCount - a.commentsCount
+      case 'Least Comments':
+        return a.commentsCount - b.commentsCount
+      default:
+        return a.id - b.id
+    }
+  }
+}
+
 function MainPage() {
+  const [sortByFilter, setSortByFilter] = useState('Most Upvotes')
+
+  const filteredFeedbackList = feedbackList.sort(
+    sortByVotesAndComments(sortByFilter)
+  )
+
   return (
     <div className='flex flex-col lg:flex-row min-h-screen w-screen lg:justify-center md:p-11 lg:p-0 lg:pt-20'>
       <Header />
       <main className='flex flex-col md:mt-8 lg:mt-0 lg:ml-8 w-full lg:w-[45rem]'>
-        <ManageBar />
+        <ManageBar
+          sortByFilter={sortByFilter}
+          setSortByFilter={setSortByFilter}
+        />
         <div className='px-4 pb-4 md:px-0 md:pb-0'>
-          {feedbackList.map((feedback) => (
+          {filteredFeedbackList.map((feedback) => (
             <Link key={feedback.id} to={`/feedback/${feedback.id}`}>
               <Card
                 title={feedback.title}
--- src/components/ManageBar.tsx
+++ src/components/ManageBar.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
 import { FaVoteYea } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import { MenuDropdown } from './MenuDropdown'
 
-function ManageBar() {
-  const [selected, setSelected] = useState('Most Upvotes')
+const filterOptions = [
+  'Most Upvotes',
+  'Least Upvotes',
+  'Most Comments',
+  'Least Comments',
+]
+
+type ManageBarProps = {
+  sortByFilter: string
+  setSortByFilter: (filter: string) => void
+}
+
+function ManageBar({ sortByFilter, setSortByFilter }: ManageBarProps) {
   return (
     <div className='bg-dark-blue md:rounded-lg p-4 flex items-center justify-between text-xs md:text-sm'>
       <div className='flex items-center w-4/5'>
@@ -12,7 +22,11 @@ function ManageBar() {
         <h1 className='text-base font-bold md:ml-3 lg:ml-3 text-white hidden md:inline-block'>
           6 Suggestions
         </h1>
-        <MenuDropdown selected={selected} setSelected={setSelected} />
+        <MenuDropdown
+          selected={sortByFilter}
+          setSelected={setSortByFilter}
+          filterOptions={filterOptions}
+        />
       </div>
       <Link to={'/feedback/add'} className='w-32'>
         <div className='rounded-xl p-2 md:p-3 bg-fuchsia-500 flex items-center justify-center hover:bg-fuchsia-400 cursor-pointer'>
