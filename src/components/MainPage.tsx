a9150c36fb0e1f702c0afef7ed7ca425f926c147
Anton Rubchenko <rubchenko.anton@gmail.com>
2022-08-20 19:36:45 +0300

    chore: Add logic to 'Sort By' filter(MainPage)

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Card } from './Card'
 import { Header } from './Header'
@@ -18,7 +19,7 @@ const feedbackList = [
     description: 'some description',
     commentsCount: 3,
     category: 'category',
-    votesCount: 34,
+    votesCount: 36,
   },
   {
     id: 3,
@@ -26,18 +27,41 @@ const feedbackList = [
     description: 'some description',
     commentsCount: 2,
     category: 'category',
-    votesCount: 34,
+    votesCount: 35,
   },
 ]
 
+function sortByVotesAndComments(sortByFilter: string) {
+  return (a: any, b: any) => {
+    switch (sortByFilter) {
+      case 'Most Upvotes':
+        return b.votesCount - a.votesCount
+      case 'Least Upvotes':
+        return a.votesCount - b.votesCount
+      case 'Most Comments':
+        return b.commentsCount - a.commentsCount
+      case 'Least Comments':
+        return a.commentsCount - b.commentsCount
+      default:
+        return a.id - b.id
+    }
+  }
+}
+
 function MainPage() {
+  const [sortByFilter, setSortByFilter] = useState('Most Upvotes')
+
+  const filteredFeedbackList = feedbackList.sort(
+    sortByVotesAndComments(sortByFilter)
+  )
+
   return (
     <div className='flex flex-col lg:flex-row min-h-screen w-screen lg:justify-center md:p-11 lg:p-0 lg:pt-20'>
       <Header />
       <main className='flex flex-col md:mt-8 lg:mt-0 lg:ml-8 w-full lg:w-[45rem]'>
-        <ManageBar />
+        <ManageBar selected={sortByFilter} setSelected={setSortByFilter} />
         <div className='px-4 pb-4 md:px-0 md:pb-0'>
-          {feedbackList.map((feedback) => (
+          {filteredFeedbackList.map((feedback) => (
             <Link key={feedback.id} to={`/feedback/${feedback.id}`}>
               <Card
                 title={feedback.title}
--- src/components/MenuDropdown.tsx
+++ src/components/MenuDropdown.tsx
@@ -4,19 +4,17 @@ import { Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 import { FaCheck } from 'react-icons/fa'
 
-const options = [
-  'Most Upvotes',
-  'Least Upvotes',
-  'Most Comments',
-  'Least Comments',
-]
-
 type MenuDropdownProps = {
   selected: string
   setSelected: (selected: string) => void
+  filterOptions: string[]
 }
 
-export function MenuDropdown({ selected, setSelected }: MenuDropdownProps) {
+export function MenuDropdown({
+  selected,
+  setSelected,
+  filterOptions,
+}: MenuDropdownProps) {
   return (
     <Menu as='div' className='relative inline-block text-left w-52 md:ml-6'>
       {({ open }) => (
@@ -44,7 +42,7 @@ export function MenuDropdown({ selected, setSelected }: MenuDropdownProps) {
             leaveTo='transform opacity-0 scale-95'
           >
             <Menu.Items className='origin-top-right absolute left-0 md:right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none'>
-              {options.map((option, idx) => (
+              {filterOptions.map((option, idx) => (
                 <Menu.Item key={idx}>
                   <div
                     className='flex justify-between items-center px-4 py-3 cursor-pointer text-base text-dark-blue hover:text-violet-600'
