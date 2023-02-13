8f96ca5843b6e59ffee1e1ccbcfdece64edb1b27
Anton Rubchenko <48853826+Antodi99@users.noreply.github.com>
2023-02-13 15:27:40 +0300

    US029: Redirect to /login page if user not logged (#37)
    * chore: Redirect to /login page if user not logged
    
    * chore: fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
+import { ReactNode, useEffect, useState } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import {
   AddFeedbackPage,
   EditFeedbackPage,
@@ -7,18 +8,76 @@ import {
   MainPage,
   LoginPage,
 } from './pages'
+import { getCurrentUser } from './services/user.service'
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    getCurrentUser().then((user) => {
+      setIsLoggedIn(!!user)
+      setIsLoading(false)
+    })
+  }, [])
+
   return (
     <Routes>
-      <Route path='/' element={<MainPage />} />
-      <Route path='/feedback/:id' element={<FeedbackPage />} />
-      <Route path='/roadmap' element={<RoadmapPage />} />
-      <Route path='/feedback/add' element={<AddFeedbackPage />} />
-      <Route path='/feedback/:id/edit' element={<EditFeedbackPage />} />
+      <Route
+        path='/'
+        element={
+          <AuthRequired isLoading={isLoading} isLoggedIn={isLoggedIn}>
+            <MainPage />
+          </AuthRequired>
+        }
+      />
+      <Route
+        path='/feedback/:id'
+        element={
+          <AuthRequired isLoading={isLoading} isLoggedIn={isLoggedIn}>
+            <FeedbackPage />
+          </AuthRequired>
+        }
+      />
+      <Route
+        path='/roadmap'
+        element={
+          <AuthRequired isLoading={isLoading} isLoggedIn={isLoggedIn}>
+            <RoadmapPage />
+          </AuthRequired>
+        }
+      />
+      <Route
+        path='/feedback/add'
+        element={
+          <AuthRequired isLoading={isLoading} isLoggedIn={isLoggedIn}>
+            <AddFeedbackPage />
+          </AuthRequired>
+        }
+      />
+      <Route
+        path='/feedback/:id/edit'
+        element={
+          <AuthRequired isLoading={isLoading} isLoggedIn={isLoggedIn}>
+            <EditFeedbackPage />
+          </AuthRequired>
+        }
+      />
       <Route path='/login' element={<LoginPage />} />
     </Routes>
   )
 }
 
 export default App
+
+type AuthRequiredProps = {
+  children: ReactNode
+  isLoggedIn: boolean
+  isLoading: boolean
+}
+
+function AuthRequired({ children, isLoggedIn, isLoading }: AuthRequiredProps) {
+  if (isLoading) return <div>Loading...</div>
+  if (!isLoggedIn) return <Navigate to='/login' />
+  return children as JSX.Element
+}
