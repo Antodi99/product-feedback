e7399e8f0410d895a9a9e9df449702c91fcf8e20
Antodi99 <rubchenko.anton@gmail.com>
2023-03-08 17:43:18 +0300

    chore: Make AddFeedback form work

--- src/components/AddFeedback.tsx
+++ src/components/AddFeedback.tsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { addFeedback } from '../services/feedback.service'
 import { Dropdown } from './Dropdown'
 
 const optionsCategory = ['UI', 'UX', 'Enhancement', 'Bug', 'Feature']
 
 function AddFeedback() {
   const [selectedCategory, setSelectedCategory] = useState('UI')
+  const [feedbackTitle, setfeedbackTitle] = useState('')
+  const [feedbackDetail, setfeedbackDetail] = useState('')
+
+  const navigate = useNavigate()
+
+  const handleAddFeedback = async () => {
+    addFeedback(feedbackDetail, feedbackTitle, selectedCategory)
+    navigate('/')
+  }
+
   return (
     <div className='bg-white flex flex-col rounded-lg p-6 h-fit w-full'>
       <h1 className='text-dark-blue text-2xl font-bold'>Create New Feedback</h1>
@@ -14,7 +25,13 @@ function AddFeedback() {
         <p className='text-dark-blue text-sm'>
           Add a short, descriptive headline
         </p>
-        <input className='bg-light-grey p-2 w-full outline-none rounded-lg mt-4'></input>
+        <input
+          className='bg-light-grey p-2 w-full outline-none rounded-lg mt-4'
+          onChange={(e) => {
+            setfeedbackTitle(e.target.value)
+          }}
+          value={feedbackTitle}
+        ></input>
       </div>
       <div className='mt-8'>
         <p className='text-dark-blue text-sm font-bold'>Category</p>
@@ -32,7 +49,13 @@ function AddFeedback() {
         <p className='text-dark-blue text-sm'>
           Include any specific comments on what should be improved, added, etc.
         </p>
-        <textarea className='bg-light-grey p-2 resize-none w-full outline-none rounded-lg mt-4 h-32'></textarea>
+        <textarea
+          className='bg-light-grey p-2 resize-none w-full outline-none rounded-lg mt-4 h-32'
+          onChange={(e) => {
+            setfeedbackDetail(e.target.value)
+          }}
+          value={feedbackDetail}
+        ></textarea>
       </div>
       <div className='flex flex-col-reverse md:flex-row md:justify-end mt-10'>
         <Link to={'/'}>
@@ -40,7 +63,10 @@ function AddFeedback() {
             <p className='text-white font-bold text-sm'>Cancel</p>
           </div>
         </Link>
-        <div className='rounded-xl p-2 md:p-3 bg-fuchsia-500 w-content md:ml-3 flex items-center justify-center hover:bg-fuchsia-400 cursor-pointer'>
+        <div
+          className='rounded-xl p-2 md:p-3 bg-fuchsia-500 w-content md:ml-3 flex items-center justify-center hover:bg-fuchsia-400 cursor-pointer'
+          onClick={handleAddFeedback}
+        >
           <p className='text-white font-bold text-sm'>Add Feedback</p>
         </div>
       </div>
