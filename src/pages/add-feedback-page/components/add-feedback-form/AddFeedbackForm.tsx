f85e3eaaee87d8fafe61c2f79987c70e2cacb06a
Antodi99 <rubchenko.anton@gmail.com>
2023-03-27 12:24:48 +0300

    fix

--- src/pages/add-feedback-page/components/add-feedback-form/AddFeedbackForm.tsx
+++ src/pages/add-feedback-page/components/add-feedback-form/AddFeedbackForm.tsx
@@ -1,23 +1,9 @@
 import { Formik } from 'formik'
-import * as Yup from 'yup'
 import { Link, useNavigate } from 'react-router-dom'
 import { addFeedback } from '../../../../services/feedback.service'
 import { Dropdown } from '../../../../components/Dropdown'
 import { optionsCategory } from './constants'
-
-const addFeedbackSchema = Yup.object().shape({
-  title: Yup.string()
-    .min(5, 'Title has to be longer then 5 characters!')
-    .max(50, 'Title has to be less then 50 characters')
-    .required('Required'),
-  detail: Yup.string()
-    .min(10, 'Description has to be longer then 10 characters')
-    .max(1000, 'Description has to be less then 1000 characters')
-    .required('Required'),
-  category: Yup.string()
-    .oneOf(optionsCategory, 'Choose category!')
-    .required('Required'),
-})
+import { addFeedbackSchema } from './validation'
 
 type FormValues = {
   detail: string
