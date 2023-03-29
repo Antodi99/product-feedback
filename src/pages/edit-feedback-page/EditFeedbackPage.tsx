55a978b8dd05157eb8c9b1e4828357103f350485
Anton Rubchenko <48853826+Antodi99@users.noreply.github.com>
2023-03-30 01:13:38 +0300

    DE010: Go Back button navigates to the Main Page (#46)
    chore: Go Back button navigates to the Main Page

--- src/pages/edit-feedback-page/EditFeedbackPage.tsx
+++ src/pages/edit-feedback-page/EditFeedbackPage.tsx
@@ -1,12 +1,13 @@
 import { FaAngleLeft } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { EditFeedback } from './components'
 
 export function EditFeedbackPage() {
+  const { id } = useParams()
   return (
     <div className='w-4/5 lg:w-4/12 flex flex-col justify-center pt-7 md:pt-20 m-auto lg:px-12 pb-7'>
       <div className='flex justify-between items-center'>
-        <Link to={'/'}>
+        <Link to={`/feedback/${id}`}>
           <div className='flex items-center hover:underline cursor-pointer'>
             <FaAngleLeft className='text-light-blue' />
             <p className='text-dark-blue font-bold text-sm ml-4'>Go Back</p>
